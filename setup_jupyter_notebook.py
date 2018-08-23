809b48ff41620cfbfcb070fb0af20753af4aac0b
jaeseongyun <yunjsjaesung@gmail.com>
2018-08-23 14:55:44 +0900

    Create setup_jupyter_notebook.py

---
+++ setup_jupyter_notebook.py
@@ -0,0 +1,21 @@
+import os
+
+home_dir = os.environ['HOME']
+os.system("jupyter notebook --generate-config")
+
+from notebook.auth import passwd
+pwsha = passwd()
+
+config_str = """
+# Server config
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'{}'
+# It is a good idea to put it on a known, fixed port
+c.NotebookApp.port = 8888
+c.NotebookApp.notebook_dir = u'/'
+c.NotebookApp.allow_root = True
+""".format(pwsha)
+
+with open(home_dir+"/.jupyter/jupyter_notebook_config.py", "w") as cf:
+    cf.write(config_str)
